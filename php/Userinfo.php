142ca7392c6890ae4276a9676f721576844ad93f
timoman7 <tc20791@sestudent.org>
2016-10-07 12:43:30 -0400

    Update Userinfo.php

--- php/Userinfo.php
+++ php/Userinfo.php
@@ -1,11 +1,11 @@
 <?php
 header("Content-type: application/x-php", true);
-$xml = simplexml_load_file("../xml/Userinfo.xml"); //In this line it create a SimpleXMLElement object with the source of the XML file. 
+$xml = simplexml_load_file("./xml/Userinfo.xml"); //In this line it create a SimpleXMLElement object with the source of the XML file. 
 $sxe = new SimpleXMLElement($xml->asXML()); //The following lines will add a new child and others child inside the previous child created
 $person = $sxe->addChild("COMMENT"); 
 $person->addChild("DATA", $params); //This next line will overwrite the original XML file with new data added 
-$sxe->asXML("../xml/Userinfo.xml");
-$retxml = simplexml_load_file('../xml/Userinfo.xml');
+$sxe->asXML("./xml/Userinfo.xml");
+$retxml = simplexml_load_file('./xml/Userinfo.xml');
 print_r($retxml);
 echo($retxml);
 ?>
