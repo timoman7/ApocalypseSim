8bcd6c6e048449c9fec442b65dcf8600fb749b92
timoman7 <tc20791@sestudent.org>
2016-10-07 13:03:24 -0400

    Update Userinfo.php

--- php/Userinfo.php
+++ php/Userinfo.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-type: application/x-php", true);
+header("Content-type: application/x-www-form-urlencoded", true);
 $xml = simplexml_load_file("./xml/Userinfo.xml"); //In this line it create a SimpleXMLElement object with the source of the XML file. 
 $sxe = new SimpleXMLElement($xml->asXML()); //The following lines will add a new child and others child inside the previous child created
 $person = $sxe->addChild("COMMENT"); 
