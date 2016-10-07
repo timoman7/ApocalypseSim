5d14c1fe5196f31379acbae070e5f2ae5ecfcea7
timoman7 <tc20791@sestudent.org>
2016-10-07 16:27:14 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -23,6 +23,7 @@ var checkStorage=function(){
           return false;
      }
 }
+setInterval(function(){if(config.getBoolean("serverText")){config.getBoolean("serverText");}},10);
 var speaking = new p5.Speech();
 speaking.setLang('en-US');
 speaking.setVoice("Google UK English Male");
