9c1321b64217af2c4a3ce79e18fa56b134806b76
timoman7 <tc20791@sestudent.org>
2016-11-03 16:23:37 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 var currentUser;
 var closePrompt = function(){
-	document.getElementById('overlay').parentNode.removeChild(document.getElementById('overlay');
+	document.getElementById('overlay').parentNode.removeChild(document.getElementById('overlay'_);
 };
 var customPrompt = function(prom){
 	if(document.getElementById('overlay')!==null){
