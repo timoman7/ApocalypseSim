cc091a89f1efd15c1a18d34c61089c98980431ea
timoman7 <tc20791@sestudent.org>
2017-01-03 09:08:11 -0500

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -18,7 +18,7 @@ var r=0;
 var m=0;
 var mousepush=false;
 var mousepush2=false;
-var charList=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "];
+var charList=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ","-"];
 var newString='';
 function keyPressed(){
     if(event.code!=="ShiftLeft"&&event.code!=="ShiftRight"){
