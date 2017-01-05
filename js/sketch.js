d40a79581029daf56fb53a8b93129126e56d85e1
timoman7 <tc20791@sestudent.org>
2017-01-05 12:11:53 -0500

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -289,7 +289,7 @@ function setup(){
 	levelGui.position(0,0);
 	levelGui.style('width',width+"px");
 	levelGui.style('height',height+"px");
-	levelGui.style("backgroundColor","DarkGreen");
+	levelGui.style("backgroundUrl",img1)
 	levelGui.id("levelGui");
 	levelGui.hide();
 	levelGuiCreate();
