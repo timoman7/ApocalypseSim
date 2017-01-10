a322d3c79a6d526a8d5428ad611b29ea98f96a1a
timoman7 <tc20791@sestudent.org>
2017-01-10 09:01:16 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,71 @@
 //Rework weapons as unique objects
 //Look around will lead to a function in sketch
 //Do this in shop
+/**
+*	Define non-native functions
+**/
+var seeded = false;
+var lcg = function() {
+  // Set to values from http://en.wikipedia.org/wiki/Numerical_Recipes
+  // m is basically chosen to be large (as it is the max period)
+  // and for its relationships to a and c
+  var m = 4294967296,
+    // a - 1 should be divisible by m's prime factors
+    a = 1664525,
+    // c and m should be co-prime
+    c = 1013904223,
+    seed, z;
+  return {
+    setSeed : function(val) {
+      // pick a random seed if val is undefined or null
+      // the >>> 0 casts the seed to an unsigned 32-bit integer
+      z = seed = (val == null ? Math.random() * m : val) >>> 0;
+    },
+    getSeed : function() {
+      return seed;
+    },
+    rand : function() {
+      // define the recurrence relationship
+      z = (a * z + c) % m;
+      // return a float in [0, 1)
+      // if z = m then z / m = 0 therefore (z % m) / m < 1 always
+      return z / m;
+    }
+  };
+};
+function randomSeed(seed) {
+  lcg.setSeed(seed);
+  seeded = true;
+}
+function random(min, max) {
+
+  var rand;
+
+  if (seeded) {
+    rand  = lcg.rand();
+  } else {
+    rand = Math.random();
+  }
+
+  if (arguments.length === 0) {
+    return rand;
+  } else
+  if (arguments.length === 1) {
+    return rand * min;
+  } else {
+    if (min > max) {
+      var tmp = min;
+      min = max;
+      max = tmp;
+    }
+
+    return rand * (max-min) + min;
+  }
+}
+
+/**
+*	Define non-native functions
+**/
 var currentUser;
 var farmGuiOpen=false;
 var Class;
