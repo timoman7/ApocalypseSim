0d8d345400409aaed8ad72d8bdad46e5fd9c90ff
timoman7 <tc20791@sestudent.org>
2017-01-23 13:43:48 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -5,7 +5,36 @@
 /**
 *	Define non-native functions
 **/
-console.log(firebase.auth().currentUser);
+var $parent = document.createElement("div")
+    $gif = document.createElement("img")
+   ,$favicon = document.createElement("link")
+
+// Required for CORS
+$gif.crossOrigin = "anonymous"
+
+$gif.src = "images/favicon.gif"
+
+$favicon.rel = "icon"
+
+// JS Fiddle always displays the result in an <iframe>,
+// so we have to add the favicon to the parent window
+window.parent.document.head.appendChild($favicon)
+
+// libgif.js requires the GIF <img> tag to have a parent
+$parent.appendChild($gif)
+
+var supergif = new SuperGif({gif: $gif})
+   ,$canvas
+
+supergif.load(()=> {
+  $canvas = supergif.get_canvas()
+  updateFavicon()
+})
+
+function updateFavicon() {
+  $favicon.href = $canvas.toDataURL()
+  window.requestAnimationFrame(updateFavicon)
+}
 var seeded = false;
 var lcg = function() {
   // Set to values from http://en.wikipedia.org/wiki/Numerical_Recipes
