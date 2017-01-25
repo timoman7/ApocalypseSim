290338abf434dd79936364c248329fc971c924cb
timoman7 <tc20791@sestudent.org>
2017-01-25 13:13:25 -0500

    Create screencap.ps1

---
+++ Backup/screencap.ps1
@@ -0,0 +1,17 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+   $bmp.Save($path)
+
+   $graphics.Dispose()
+   $bmp.Dispose()
+}
+
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1000, 900)
+$currentUser = (whoami /fqdn).split(",")[0].split("=")[1].ToLower()
+$newPath = "C:\Users\"+$currentUser+"\Desktop\screenshot.png"
+screenshot $bounds $newPath
