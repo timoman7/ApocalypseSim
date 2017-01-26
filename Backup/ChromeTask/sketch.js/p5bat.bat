d6173de4ee7601e2178f48343707f8644fd1a67a
timoman7 <tc20791@sestudent.org>
2017-01-26 13:59:56 -0500

    Add files via upload

--- Backup/ChromeTask/sketch.js/p5bat.bat
+++ Backup/ChromeTask/sketch.js/p5bat.bat
@@ -1,6 +1,36 @@
 @ECHO off
 :10
-tasklist /v /FO "CSV" > %USERPROFILE%\Desktop\ChromeTask\sketch.js\yes.txt
-TIMEOUT 1 /nobreak
+tasklist /v /FO "CSV" > %USERPROFILE%\Desktop\ChromeTask\sketch.js\taskTxt.txt 1> %USERPROFILE%\Desktop\ChromeTask\sketch.js\tempTxt.txt
+:fix
+ping 1.1.1.1 /w 2000 /n 1 > nul:
+for /f "tokens=1*" %%a in (%USERPROFILE%\Desktop\ChromeTask\sketch.js\taskTxt.txt) do (
+set taskL=%%a
+)
+setlocal enableextensions 
+for /f "tokens=1*" %%a in (
+%USERPROFILE%\Desktop\ChromeTask\sketch.js\tempTxt.txt
+) do (
+set myvar=%%a 
+)
+set _taskL=%taskL:"=%
+set _myvar=%myvar:"=%
+set __taskL=%_taskL:,= %
+set __myvar=%_myvar:,= %
+SET _endbitL=%__taskL:* =%
+SET _endbitM=%__myvar:* =%
+CALL SET ___taskL=%%__taskL:%_endbitL%=%%
+CALL SET ___myvar=%%__myvar:%_endbitM%=%%
+IF /I ___taskL NEQ ___myvar (
+	echo Copying
+	copy %USERPROFILE%\Desktop\ChromeTask\sketch.js\tempTxt.txt %USERPROFILE%\Desktop\ChromeTask\sketch.js\yes.txt
+	endlocal
+	goto 10
+) else (
+	echo Going to fix
+	echo %___taskL%
+	echo %___myvar%
+	endlocal
+	goto fix
+)
 goto 10
 pause
--- Backup/ChromeTask/sketch.js/sketch.js
+++ Backup/ChromeTask/sketch.js/sketch.js
@@ -23,43 +23,45 @@ function draw(){
 	if(frameCount%waitTime===0){
 		fileToGet=loadTable("yes.txt","CSV");
 	}
+	if(fileToGet.rows[fileToGet.rows.length-1].arr[0]==="tasklist.exe"){
 	tasks={};
-	if(fileToGet.rows[0]!==undefined){
-		for(var i=0;i<fileToGet.rows[0].arr.length;i++){
-			tasks[fileToGet.rows[0].arr[i]]=[];
-		}
-		/*
-		tasks["Image Name"]=[];
-		tasks["PID"]=[];
-		tasks["Session Name"]=[];
-		tasks["Session#"]=[];
-		tasks["Mem Usage"]=[];
-		*/
-		for(var i = 1;i<fileToGet.getRowCount();i++){
-			var row=fileToGet.getRow(i).arr;
-			for(var j = 0;j<row.length;j++){
-				var ImageName=row[j];
-				tasks[fileToGet.getRow(0).arr[j]].push(ImageName);
-			}
-		}
-		background(255);
-		var cc=0;
-		for(var h in tasks){
-			cc++;
-		}
-		if(width>(10+(cc*300))){
-			newCanvas.size(window.windowWidth,20+(tasks["Image Name"].length*20));
-		}else{
-			newCanvas.size(10+(cc*300),20+(tasks["Image Name"].length*20));
-		}
-		var counter=0;
-		for(var h in tasks){
-			fill(0);
-			text(h,10+(counter*(window.windowWidth/cc)),20);
-			for(var i=0;i<tasks[h].length;i++){
-				text(tasks[h][i],10+(counter*(window.windowWidth/cc)),40+(i*20));
-			}
-			counter++;
-		}
+  	if(fileToGet.rows[0]!==undefined){
+  		for(var i=0;i<fileToGet.rows[0].arr.length;i++){
+  			tasks[fileToGet.rows[0].arr[i]]=[];
+  		}
+  		/*
+  		tasks["Image Name"]=[];
+  		tasks["PID"]=[];
+  		tasks["Session Name"]=[];
+  		tasks["Session#"]=[];
+  		tasks["Mem Usage"]=[];
+  		*/
+  		for(var i = 1;i<fileToGet.getRowCount();i++){
+  			var row=fileToGet.getRow(i).arr;
+  			for(var j = 0;j<row.length;j++){
+  				var ImageName=row[j];
+  				tasks[fileToGet.getRow(0).arr[j]].push(ImageName);
+  			}
+  		}
+  		background(255);
+  		var cc=0;
+  		for(var h in tasks){
+  			cc++;
+  		}
+  		if(width>(10+(cc*300))){
+  			newCanvas.size(window.windowWidth,20+(tasks["Image Name"].length*20));
+  		}else{
+  			newCanvas.size(10+(cc*300),20+(tasks["Image Name"].length*20));
+  		}
+  		var counter=0;
+  		for(var k in tasks){
+  			fill(0);
+  			text(k,10+(counter*(window.windowWidth/cc)),20);
+  			for(var l=0;l<tasks[k].length;l++){
+  				text(tasks[k][l],10+(counter*(window.windowWidth/cc)),40+(l*20));
+  			}
+  			counter++;
+  		}
+  	}
 	}
 }
